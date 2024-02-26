ef5f66faeced993322d61fac62436bb793c24670
RobertMcRae <robert.mcrae@mail.mcgill.ca>
2024-02-26 08:39:53 -0500

    Delete method implemented

--- backend/recipe.py
+++ backend/recipe.py
@@ -36,3 +36,25 @@ def create_recipe():
     db.session.commit()
 
     return format_recipe(new_recipe)
+
+@recipe.route('/deleteRecipe', methods=['DELETE'])
+def delete_recipe():
+    try:
+        #deleting recipe based on the name given to the recipe
+        data = request.json
+        name = data.get("name") 
+        
+        deleted_recipe = Recipe.query.filter_by(name=name).first()
+        
+        #will only delete recipe from db if a recipe is found by the name
+        if deleted_recipe:
+            db.session.delete(deleted_recipe)
+            db.session.commit()
+        else: 
+            return jsonify({'message': 'The recipe with the given name cannot be found'}), 400
+
+        return format_recipe(deleted_recipe) 
+    
+    except Exception as e: 
+        return jsonify({'message': str(e)}), 500
+    
