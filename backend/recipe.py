c766110236cdc11a0cff07b279d728637d1fc2d7
khc1100 <97474403+khc1100@users.noreply.github.com>
2024-02-27 12:56:25 -0500

    added picture to recipe.py

--- backend/recipe.py
+++ backend/recipe.py
@@ -11,7 +11,8 @@ def format_recipe(recipe):
         "servingSize": recipe.servingSize,
         "description": recipe.description,
         "origin": recipe.origin,
-        "category": recipe.category
+        "category": recipe.category,
+        "image_url": recipe.image_url 
         
     }
 
@@ -36,3 +37,25 @@ def create_recipe():
     db.session.commit()
 
     #return format_recipe(new_recipe)
+
+# Add picture to recipe
+#https://healthyfitnessmeals.com/wp-content/uploads/2021/02/Honey-garlic-chicken-meal-prep-9.jpg 
+@recipe.route('/addPicture', methods=['PUT'])
+def add_picture_to_recipe(recipe_id):
+    try:
+        #Add picture to recipe based on the name given to the recipe
+        data = request.json
+        name = data.get("name") 
+        
+        updated_recipe = Recipe.query.filter_by(name=name).first()
+        #will only add picture recipe from db if a recipe is found by the name
+        if updated_recipe:
+            data = request.json
+            image_url = data.get('image_url')
+            recipe.image_url = image_url
+            db.session.commit()
+        else: 
+            return jsonify({'message': 'The recipe with the given name cannot be found'}), 400
+
+    except Exception as e: 
+        return jsonify({'message': str(e)}), 500
