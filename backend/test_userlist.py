07599ec2b4823df0b56453dbcf4281a5f68a4ad5
Thomas Song <songtom96@hotmail.com>
2024-02-26 22:12:24 -0500

    adding unit test for userlist

---
+++ backend/test_userlist.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+sys.path.append('../')
+from app import app, db 
+
+class TestSearchUsers(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        
+        # Create all database tables
+        with app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        # Remove all database tables
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    # Unit test 1: Success User Registration (Normal Flow)
