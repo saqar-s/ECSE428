c06036de02692223969d5cbc07b5de78d8df7d2a
Thomas Song <songtom96@hotmail.com>
2024-04-01 11:39:41 -0400

    Update test_all.py

--- backend/tests/test_all.py
+++ backend/tests/test_all.py
@@ -6,6 +6,7 @@ from test_register_user import TestRegisterUser
 from test_userlist import TestUserlist
 from test_create_recipe import TestCreateRecipe
 from test_add_to_calendar import TestAddToCalendar
+from test_remove_from_calendar import TestRemoveFromCalendar
 
 test_suite = unittest.TestSuite()
 
@@ -16,5 +17,6 @@ test_suite.addTest(unittest.makeSuite(TestRegisterUser))
 test_suite.addTest(unittest.makeSuite(TestUserlist))
 test_suite.addTest(unittest.makeSuite(TestCreateRecipe))
 test_suite.addTest(unittest.makeSuite(TestAddToCalendar))
+test_suite.addTest(unittest.makeSuite(TestRemoveFromCalendar))
 
 unittest.TextTestRunner().run(test_suite)
